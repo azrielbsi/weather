5f8b4f2e8eef2b1171d890b2cf2e7cb54954178d
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2023-12-28 18:23:32 +0700

    Update update_readme.sh

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -18,10 +18,21 @@ weather_info=$(curl -s "http://api.openweathermap.org/data/2.5/weather?q=${city}
 temperature_kelvin=$(echo $weather_info | jq -r '.main.temp')
 temperature_celsius=$(kelvin_to_celsius $temperature_kelvin)
 condition=$(echo $weather_info | jq -r '.weather[0].description')
+temp_min_kelvin=$(echo $weather_info | jq -r '.main.temp_min')
+temp_max_kelvin=$(echo $weather_info | jq -r '.main.temp_max')
+humidity=$(echo $weather_info | jq -r '.main.humidity')
+wind_speed=$(echo $weather_info | jq -r '.wind.speed')
+
+temp_min_celsius=$(kelvin_to_celsius $temp_min_kelvin)
+temp_max_celsius=$(kelvin_to_celsius $temp_max_kelvin)
 
 echo "# My Project" > README.md
 echo -e "\nThis content is dynamically generated in Indonesian Time (IST): $time\n" >> README.md
 echo -e "\nCurrent Weather in $city:\nTemperature: $temperature_celsius °C\nCondition: $condition" >> README.md
+echo "Suhu Maksimum: ${temp_max_celsius}°C"
+echo "Suhu Minimum: ${temp_min_celsius}°C"
+echo "Kelembaban: ${humidity}%"
+echo "Kecepatan Angin: ${wind_speed} m/s"
 
 git config --global user.email "action@github.com"
 git config --global user.name "GitHub Action"
