81b2665233316258469da4422bde0a382a4e9ff4
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2023-12-28 18:44:44 +0700

    Update update_readme.sh

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -11,6 +11,7 @@ kelvin_to_celsius() {
 }
 
 time=$(date +'%Y-%m-%d %H:%M:%S %Z')
+city=${CITY}
 weather_info=$(curl -s "http://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${OPENWEATHERMAP_API_KEY}")
 
 temperature_kelvin=$(echo $weather_info | jq -r '.main.temp')
