35d803e4996966794489d28a193c758562002632
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2023-12-28 16:58:42 +0700

    Update update_readme.sh

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -4,7 +4,7 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export LC_TIME=en_US.UTF-8
-export TZ=Asia/Kolkata
+export TZ=Asia/Jakarta
 
 kelvin_to_celsius() {
     echo "scale=2; $1 - 273.15" | bc
