99ffadc000ee80899361241c24f18a94590dc704
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2023-12-29 15:10:02 +0700

    Update update_readme.sh

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -39,6 +39,9 @@ sunset_unix=$(echo $weather_info | jq -r '.sys.sunset')
 sunrise_readable=$(date -d @$sunrise_unix +'%Y-%m-%d %H:%M:%S')
 sunset_readable=$(date -d @$sunset_unix +'%Y-%m-%d %H:%M:%S')
 timezone=$(echo $weather_info | jq -r '.timezone')
+timestamp=$(echo $weather_info | jq -r '.dt')
+formatted_time=$(date -d @$timestamp +'%Y-%m-%d %H:%M:%S')
+
 
 echo "# <h1 align="center"><img height="40" src="images/cloud.png"> Daily Weather <img height="40" src="images/cloud.png"></h1>" > README.md
 echo -e "<h3 align="center">🕒 Indonesian Time(UTC +07:00): <u>$time</u> (🤖Automated)</h3>\n" >> README.md
@@ -72,6 +75,7 @@ echo -e "<td>Sunset: ${sunset_readable}</td>" >> README.md
 echo -e "</tr>" >> README.md
 echo -e "<tr>" >> README.md
 echo -e "<td>Timezone: UTC$(printf "%+.2f" "$(bc <<< "scale=2; $timezone / 3600")")</td>" >> README.md 
+echo -e "<td>Time: ${formatted_time}</td>" >> README.md
 echo -e "</tr>" >> README.md
 echo -e "</table>" >> README.md
 echo -e "</table>" >> README.md
