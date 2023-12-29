8de4bcd2c2ba929fe0b6ed3840298ad27dd99384
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2023-12-29 14:03:19 +0700

    Update update_readme.sh

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -25,6 +25,7 @@ wind_speed=$(echo $weather_info | jq -r '.wind.speed')
 feels_like_kelvin=$(echo $weather_data | jq -r '.main.feels_like')
 feels_like_celsius=$(kelvin_to_celsius $feels_like_kelvin)
 pressure=$(echo $weather_info | jq -r '.main.pressure')
+sea_level=$(echo $weather_info | jq -r '.main.sea_level')
 
 temp_min_celsius=$(kelvin_to_celsius $temp_min_kelvin)
 temp_max_celsius=$(kelvin_to_celsius $temp_max_kelvin)
@@ -49,7 +50,10 @@ echo -e "<td><img src="images/humidity.png" height="18"> Humidity: <b>${humidity
 echo -e "<td><img src="images/air-flow.png" height="18"> Wind Speed: <b>${wind_speed} m/s</b></td>" >> README.md
 echo -e "</tr>" >> README.md
 echo -e "<tr>" >> README.md
-echo -e "<td>${pressure} hPa</td>" >> README.md
+echo -e "<td>Tekanan Udara: ${pressure} hPa</td>" >> README.md
+echo -e "</tr>" >> README.md
+echo -e "<tr>" >> README.md
+echo -e "<td>Sea Level: ${sea_level} hPa</td>" >> README.md
 echo -e "</tr>" >> README.md
 echo -e "</table>" >> README.md
 echo -e "</table>" >> README.md
