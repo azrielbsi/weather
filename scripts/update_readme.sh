429e58837a50db8b10429394e77ee3b5f4b96904
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2023-12-30 04:38:01 +0700

    Update update_readme.sh

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -11,7 +11,7 @@ kelvin_to_celsius() {
 }
 
 time=$(date +'%Y-%m-%d %H:%M:%S %Z')
-location=$(curl -s https://ipinfo.io/${{ secrets.IP }}/json?token=${{ secrets.IPTOKEN }})
+location=$(curl -s https://ipinfo.io/${IP}/json?token=${IPTOKEN})
 city=$(echo "$location" | jq -r '.city')
 city_encoded=${city// /%20}
 weather_info=$(curl -s "http://api.openweathermap.org/data/2.5/weather?q=${city_encoded}&appid=${OPENWEATHERMAP_API_KEY}")
