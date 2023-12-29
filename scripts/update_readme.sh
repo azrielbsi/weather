59e098d6c67ef9cccb50a5548283ee1c41e0d956
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2023-12-30 03:01:22 +0700

    Update update_readme.sh

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -46,7 +46,6 @@ sunrise_readable=$(date -d @$sunrise_unix +'%Y-%m-%d %H:%M:%S')
 sunset_readable=$(date -d @$sunset_unix +'%Y-%m-%d %H:%M:%S')
 timezone=$(echo $weather_info | jq -r '.timezone')
 grnd_level=$(echo "$weather_info" | jq -r '.main.grnd_level')
-if [ "$grnd_level" != "null" ]; then
 
 echo "# <h1 align="center"><img height="40" src="images/cloud.png"> Daily Weather Report <img height="40" src="images/cloud.png"></h1>" > README.md
 echo -e "<h3 align="center">🕒 Indonesian Time(UTC$(printf "%+.2f" "$(bc <<< "scale=2; $timezone / 3600")")): <u>$time</u> (🤖Automated)</h3>\n" >> README.md
