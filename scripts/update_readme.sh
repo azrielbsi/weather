3b2bdc36eb49dc91f1896041e297fa1f9896c7e9
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2023-12-29 18:10:25 +0700

    Update update_readme.sh

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -40,7 +40,6 @@ sunset_unix=$(echo $weather_info | jq -r '.sys.sunset')
 sunrise_readable=$(date -d @$sunrise_unix +'%Y-%m-%d %H:%M:%S')
 sunset_readable=$(date -d @$sunset_unix +'%Y-%m-%d %H:%M:%S')
 timezone=$(echo $weather_info | jq -r '.timezone')
-forecast_info=$(curl -s "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=57632351a75bb6b4af946f2ddd2453f3")
 
 echo "# <h1 align="center"><img height="40" src="images/cloud.png"> Daily Weather Report <img height="40" src="images/cloud.png"></h1>" > README.md
 echo -e "<h3 align="center">🕒 Indonesian Time(UTC$(printf "%+.2f" "$(bc <<< "scale=2; $timezone / 3600")")): <u>$time</u> (🤖Automated)</h3>\n" >> README.md
@@ -69,9 +68,6 @@ echo -e "</tr>" >> README.md
 echo -e "<tr>" >> README.md
 echo -e "<td align="center"><img src="images/sunrise.png" height="25"><br>Sunrise:<br><b>${sunrise_readable}</b></td>" >> README.md
 echo -e "<td align="center"><img src="images/sunsets.png" height="25"><br>Sunset:<br><b>${sunset_readable}</b></td>" >> README.md
-echo -e "</tr>" >> README.md
-echo -e "<tr>" >> README.md
-echo -e "<td align="center" colspan="2">${forecast_info}</td>" >> README.md
 echo -e "</tr>" >> README.md
 echo -e "</table>" >> README.md
 echo -e "</table>" >> README.md
