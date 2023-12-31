55d93ecd88885f26ccd0b6c902f928ad82384dcb
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2024-01-01 06:28:06 +0700

    Update update_readme.sh

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -102,6 +102,26 @@ echo -e "<td align='center'><img src='images/placeholder.png' height='18'> <b>${
 echo -e "</tr>" >> README.md
 echo -e "<td>" >> README.md
 echo -e "<table align="center">" >> README.md
+current_time=$(date +'%Y-%m-%d %H:%M:%S')
+twenty_four_hours_later=$(date -d "+24 hours" +'%Y-%m-%d %H:%M:%S')
+echo -e "<table align="center">" >> README.md
+echo -e "<tr>" >> README.md
+
+for ((i=0; i<8; i++)); do
+    forecast_date_unix=$(echo "$forecast_24h_info" | jq -r ".list[$i].dt")
+    forecast_date_readable=$(date -d @$forecast_date_unix +'%Y-%m-%d %H:%M:%S')
+
+    if [[ $forecast_date_readable > $current_time && $forecast_date_readable < $twenty_four_hours_later ]]; then
+        forecast_condition=$(echo "$forecast_24h_info" | jq -r ".list[$i].weather[0].main")
+        forecast_temperature_kelvin=$(echo "$forecast_24h_info" | jq -r ".list[$i].main.temp")
+        forecast_temperature_celsius=$(kelvin_to_celsius $forecast_temperature_kelvin)
+        weather_icon_code=$(echo "$forecast_24h_info" | jq -r ".list[$i].weather[0].icon")
+
+        icon_url="https://openweathermap.org/img/w/${weather_icon_code}.png"
+
+        echo -e "<td align="center"><b>${forecast_temperature_celsius:-0}°C</b><br><img src='$icon_url' height='50'><br><b>$forecast_condition</b><br><b>${forecast_date_readable:11:5}</b></td>" >> README.md        
+    fi
+done
 echo -e "<tr>" >> README.md
 echo -e "<td align="center" colspan="2"><img src="images/rain.png" height="25"><br>Rainfall: <b>${rainfall_mm:-0} Millimeters</b></td>" >> README.md
 echo -e "</tr>" >> README.md
@@ -131,7 +151,7 @@ echo "<h2>Next 24 Hours Forecast</h2>" >> README.md
 
 current_time=$(date +'%Y-%m-%d %H:%M:%S')
 twenty_four_hours_later=$(date -d "+24 hours" +'%Y-%m-%d %H:%M:%S')
-echo -e "<table align="center" style='width: 80%; border-collapse: collapse;'>" >> README.md
+echo -e "<table align="center">" >> README.md
 echo -e "<tr>" >> README.md
 
 for ((i=0; i<8; i++)); do
