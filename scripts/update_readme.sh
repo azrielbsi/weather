0e450117cbf06f9d346d42d60bf7f08fb142beaf
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2024-01-01 05:00:23 +0700

    Update update_readme.sh

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -132,18 +132,26 @@ echo "<h2>5-Day Forecast</h2>" >> README.md
 start_date=$(date +%Y-%m-%d)
 end_date=$(date -d "$start_date +4 days" +%Y-%m-%d)
 
-for ((i=0; i<5; i++)); do
+echo -e "| Date       | Condition              | Temperature   |" >> README.md
+echo -e "|------------|------------------------|---------------|" >> README.md
+
+for ((i=0; i<40; i++)); do
     forecast_date_unix=$(echo "$forecast_info" | jq -r ".list[$i].dt")
     forecast_date_readable=$(date -d @$forecast_date_unix +'%Y-%m-%d')
 
-    forecast_condition=$(echo "$forecast_info" | jq -r ".list[$i].weather[0].description")
-    forecast_temperature_kelvin=$(echo "$forecast_info" | jq -r ".list[$i].main.temp")
-    forecast_temperature_celsius=$(kelvin_to_celsius $forecast_temperature_kelvin)
+    if [[ $forecast_date_readable == $start_date ]]; then
+        forecast_condition=$(echo "$forecast_info" | jq -r ".list[$i].weather[0].description")
+        forecast_temperature_kelvin=$(echo "$forecast_info" | jq -r ".list[$i].main.temp")
+        forecast_temperature_celsius=$(kelvin_to_celsius $forecast_temperature_kelvin)
 
-    echo -e "<h3>$forecast_date_readable</h3>" >> README.md
-    echo -e "<p><b>Condition:</b> $forecast_condition</p>" >> README.md
-    echo -e "<p><b>Temperature:</b> ${forecast_temperature_celsius:-0}°C</p>" >> README.md
-    echo -e "<hr>" >> README.md
+        echo -e "| $forecast_date_readable | $forecast_condition | ${forecast_temperature_celsius:-0}°C |" >> README.md
+
+        start_date=$(date -d "$start_date +1 days" +%Y-%m-%d)
+    fi
+
+    if [[ $forecast_date_readable == $end_date ]]; then
+        break
+    fi
 done
 
 git config --global user.email "action@github.com"
