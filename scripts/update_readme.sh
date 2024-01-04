021a54299d05b1cb28f761f44c3202ecb4a6a40e
azrielbsi <126305178+azrielbsi@users.noreply.github.com>
2024-01-04 11:35:38 +0700

    Update update_readme.sh

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -156,7 +156,7 @@ echo -e "</tr>" >> README.md
 echo -e "</table>" >> README.md
 echo -e "<h2>📄 License</h2>" >> README.md
 echo -e "<li>Powered by: <a href="https://github.com/azrielbsi/weather">Daily-Weather-Report</a></li>" >> README.md
-echo -e "<li><a href="https://opensource.org/licenses/MIT"><img src="https://img.shields.io/badge/License-MIT-yellow.svg"></a></li>" >> README.md
+echo -e "<li><a href="https://github.com/azrielbsi/weather/blob/main/LICENSE"><img src="https://img.shields.io/badge/License-MIT-yellow.svg"></a></li>" >> README.md
 
 git config --global user.email "19215261@bsi.ac.id"
 git config --global user.name "${{ github.repository_owner }}"
